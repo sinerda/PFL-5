9e4031e0693d8ff03489a2c1b08a1b031f804755
Andrey Sinyavskiy <sinerda@gmail.com>
2022-11-16 01:31:38 +0200

    essence + practice

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-
+  $('.practice__info-btn').on('mouseover', function () {
+    $(this).toggleClass('active');
+  })
+  $('.practice__info-btn').on('mouseout', function () {
+    $(this).toggleClass('active');
+  })
 })
