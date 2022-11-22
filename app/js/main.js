f786b34a7d4706018e5ed93179c6532420207bf6
Andrey Sinyavskiy <sinerda@gmail.com>
2022-11-22 11:58:57 +0200

    location, progress, people, partners

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,16 @@ $(function () {
   $('.practice__info-btn').on('mouseout', function () {
     $(this).toggleClass('active');
   })
+
+  $('.partners__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // infinite: true,
+    speed: 500,
+    // autoplay: true,
+    autoplaySpeed: 4500,
+    variableWidth: true,
+    rows: 0,
+    centerMode: true,
+  });
 })
