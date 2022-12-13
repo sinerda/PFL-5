0200ce11fb5acb85b7b8d5b31800c407c13d35d8
Andrey Sinyavskiy <sinerda@gmail.com>
2022-12-13 19:17:53 +0200

    mobile: essence + practice

--- app/js/main.js
+++ app/js/main.js
@@ -24,8 +24,28 @@ $(function () {
     $('.header-menu').toggleClass('active');
   })
 
-  $(".donate-money__tabs").tabs();
+  // Редактируем текст внутри HTML (убираем лишние пробелы), чтобы красиво смотрелся на мобайле
+  const _487px = window.matchMedia("(max-width: 487px)");
+  window.onresize = function () {
+    const title = $('.practice__info-block-title');
+    const text = $('.practice__info-block-text');
 
+    if (_487px.matches) {
+      // Исправленный текст для мобильной версии
+      title.html('Інвестор (донор)');
+      text.html('Інвестор (донор) вкладає власні кошти у купівлю модульного будинку + підписання офіційного договору.');
+    }
+    else {
+      // Оригинал
+      title.html('Інвестор ( донор )');
+      text.html('Інвестор ( донор ) вкладає власні кошти у купівлю модульного будинку + підписання офіційного договору.');
+    }
+  }
+
+
+  // 
+  // НЕ ДОДЕЛАЛ. НЕ РАБОТАЕТ!
+  $(".donate-money__tabs").tabs();
   let
     tab_link = $('.donate-money__list-tabs-link'),
     tab = $('.donate-money__tab-item'),
